80f0eb5345e8ad5f42af9f6229e865e1d6b6ffbb
John Otander <johnotander@gmail.com>
2015-01-12 15:31:22 -0700

    Add test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,18 @@
+var fs = require('fs');
 var assert = require('assert');
+var rework = require('rework');
 var reworkOnlyChild = require('..');
 
 describe('rework-only-child', function() {
 
-  it('should do something awesome', function() {
-    assert.equal(camelizedPackageName(), true);
+  it('correctly convert :only-child to :first-child:last-child', function() {
+    var output = rework(fixture('only-child.css')).use(reworkOnlyChild()).toString().trim();
+    var expected = fixture('only-child.expected.expected');
+
+    assert.equal(output, expected);
   });
 });
+
+function fixture(name) {
+  return fs.readFileSync('test/fixtures/' + name, 'utf8').trim();
+}
